79f04c628fc8bc892b573a608b144e7ebb1b0d96
SokolX <89soek@gmail.com>
2020-02-24 21:59:59 +0100

    feat: add line-through and disabled link when no demo version project

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ fetch("https://api.github.com/users/sokolx/repos?+sort:updated-date-asc")
   for (const repo of repos) {
     console.log(repos)
     const {name, html_url, homepage, description, language} = repo;
-
+    
     if (language == 'CSS' || language == 'HTML' || language == 'JavaScript' ) {
 
       list.innerHTML += `<li class="projects__item">
@@ -34,13 +34,13 @@ fetch("https://api.github.com/users/sokolx/repos?+sort:updated-date-asc")
         <span style="font-size: 3rem;">
           <i class="fa fa-github"></i>
         </span>
-        <h4 class="projects__title">${name}</h4>
+        <h4 class="projects__name">${name}</h4>
         <p class="projects__description">${description ? description : "Brak opisu projektu. " }</p>
       </section>
 
       <footer class="projects__footer">
 
-          <a class="projects__links" href=${homepage}>
+          <a class="projects__links ${homepage ? homepage : "projects__links--inactive" }" href=${homepage}>
             <img class="projects__image" src="/assets/img/img-demo.svg">Demo
           </a>
           <a class="projects__links projects__links--center">
@@ -49,7 +49,7 @@ fetch("https://api.github.com/users/sokolx/repos?+sort:updated-date-asc")
           <a class="projects__links" href=${html_url}>
             <img class="projects__image" src="/assets/img/img-git.svg">Github
           </a>
-          
+
       </footer>
 
     </li>`
